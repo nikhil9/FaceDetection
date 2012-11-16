c172c704ce40fbdd9cbf27ee3233374ed1886d58
nikhil <nikhil.spitfire@gmail.com>
2012-11-16 19:59:15 +0530

    Code added

---
+++ src/FaceDetection.java
@@ -0,0 +1,59 @@
+import com.googlecode.javacv.cpp.opencv_core.IplImage;
+import static com.googlecode.javacv.cpp.opencv_core.*;
+import static com.googlecode.javacv.cpp.opencv_highgui.*;
+import static com.googlecode.javacv.cpp.opencv_objdetect.*;
+
+public class FaceDetection{
+	
+	public static final String XML_FILE = 
+			"resources/haarcascade_frontalface_default.xml";
+	
+	public static void main(String[] args){
+		
+		IplImage img = cvLoadImage("resources/lena.jpg");		
+		detect(img);		
+	}	
+	
+	public static void detect(IplImage src){
+		CvHaarClassifierCascade cascade = new 
+				CvHaarClassifierCascade(cvLoad(XML_FILE));
+		CvMemStorage storage = CvMemStorage.create();
+		CvSeq sign = cvHaarDetectObjects(
+				src,
+				cascade,
+				storage,
+				1.5,
+				3,
+				CV_HAAR_DO_CANNY_PRUNING);
+		
+		cvClearMemStorage(storage);
+		
+		int total_objects = sign.total();
+		
+		if(total_objects > 0){
+			System.out.println("detected " + total_objects + " objects");
+			
+		}
+		else{
+			System.out.println("not detected");	
+			
+		}
+		
+		for(int i = 0; i < total_objects; i++){
+			CvRect r = new CvRect(cvGetSeqElem(sign, i));
+			cvRectangle (
+					src,
+					cvPoint(r.x(), r.y()),
+					cvPoint(r.width() + r.x(), r.height() + r.y()),
+					CvScalar.RED,
+					2,
+					CV_AA,
+					0);
+			
+		}
+		
+		cvShowImage("Result", src);
+		cvWaitKey(0);
+		
+	}			
+}
